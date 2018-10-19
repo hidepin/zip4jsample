3f078a029e9b0545159381806db3b34086b19bf5
Hidetoshi Imai <hidepin@gmail.com>
2018-10-20 01:23:36 +0900

    Add ZipLoop

---
+++ src/main/java/org/func/sample/zip4jsample/ZipLoop.java
@@ -0,0 +1,13 @@
+package org.func.sample.zip4jsample;
+
+public class ZipLoop {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("error");
+			System.exit(1);
+		}
+
+	}
+
+}
