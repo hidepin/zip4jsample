60900501869daf39ff44263d247699bf3908a653
Hidetoshi Imai <hidepin@gmail.com>
2018-10-20 18:12:29 +0900

    Update zip

--- src/main/java/org/func/sample/zip4jsample/ZipUtil.java
+++ src/main/java/org/func/sample/zip4jsample/ZipUtil.java
@@ -18,77 +18,63 @@ import net.lingala.zip4j.util.Zip4jConstants;
 
 public class ZipUtil {
 
-    private int compressionMethod = Zip4jConstants.COMP_DEFLATE;
-    private int compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL;
-    private int encryptionMethod = Zip4jConstants.ENC_METHOD_STANDARD;
-    private int aesKeyStrength = Zip4jConstants.AES_STRENGTH_256;
+	private int compressionMethod = Zip4jConstants.COMP_DEFLATE;
+	private int compressionLevel = Zip4jConstants.DEFLATE_LEVEL_NORMAL;
+	private int encryptionMethod = Zip4jConstants.ENC_METHOD_STANDARD;
+	private int aesKeyStrength = Zip4jConstants.AES_STRENGTH_256;
 	private ZipOutputStream outputStream;
 
-    public ZipUtil() {
-    }
+	public ZipUtil() {
+	}
 
-    public ZipUtil(int encryptionMethod, int aesKeyStrength) {
-        this.encryptionMethod = encryptionMethod;
-        this.aesKeyStrength = aesKeyStrength;
-    }
+	public ZipUtil(int encryptionMethod, int aesKeyStrength) {
+		this.encryptionMethod = encryptionMethod;
+		this.aesKeyStrength = aesKeyStrength;
+	}
 
-    public void zip(String input, String output, String password)
-            throws ZipException, IOException, CloneNotSupportedException {
-        zip(input, output, password, "");
-    }
+	public void zip(String input, String output, String password)
+			throws ZipException, IOException, CloneNotSupportedException {
+		zip(input, output, password, "");
+	}
 
-    public void zip(String input, String output, String password, String fileNameCharset)
-            throws ZipException, IOException, CloneNotSupportedException {
+	public void zip(String input, String output, String password, String fileNameCharset)
+			throws ZipException, IOException, CloneNotSupportedException {
 
-        ZipModel zipModel = new ZipModel();
-        // If the fileNameCharset is empty then charset is detected automatically
-        // Try with Cp850 and UTF8 or OS default
-        if (!fileNameCharset.isEmpty()) {
-            zipModel.setFileNameCharset(fileNameCharset);
-        }
+		ZipModel zipModel = new ZipModel();
+		// If the fileNameCharset is empty then charset is detected automatically
+		// Try with Cp850 and UTF8 or OS default
+		if (!fileNameCharset.isEmpty()) {
+			zipModel.setFileNameCharset(fileNameCharset);
+		}
 
-        ZipParameters parameters = new ZipParameters();
-        parameters.setCompressionMethod(compressionMethod);
-        parameters.setCompressionLevel(compressionLevel);
-        parameters.setEncryptFiles(true);
-        parameters.setEncryptionMethod(encryptionMethod);
-        parameters.setAesKeyStrength(aesKeyStrength);
-        parameters.setPassword(password);
-        parameters.setDefaultFolderPath(new NativeStorage(new File(input)));
+		ZipParameters parameters = new ZipParameters();
+		parameters.setCompressionMethod(compressionMethod);
+		parameters.setCompressionLevel(compressionLevel);
+		parameters.setEncryptFiles(true);
+		parameters.setEncryptionMethod(encryptionMethod);
+		parameters.setAesKeyStrength(aesKeyStrength);
+		parameters.setPassword(password);
+		parameters.setDefaultFolderPath(new NativeStorage(new File(input)));
 
-        try {
-            outputStream = new ZipOutputStream(new NativeFile(new File(output), "rw"), zipModel);
-            LinkedList<File> remained = new LinkedList();
-            remained.add(new File(input));
-            while (!remained.isEmpty()) {
-                File file = remained.pollFirst();
-                outputStream.putNextEntry(new NativeStorage(file), (ZipParameters)parameters.clone());
-                if (file.isDirectory()) {
-                    remained.addAll(0, Arrays.asList(file.listFiles()));
-                } else {
-                    try (
-                        InputStream inputStream = new FileInputStream(file)
-                    ){
-                        byte[] readBuff = new byte[4096];
-                        int readLen = -1;
-                        while ((readLen = inputStream.read(readBuff)) != -1) {
-                            outputStream.write(readBuff, 0, readLen);
-                        }
-                    }
-                }
-                outputStream.closeEntry();
-            }
-            outputStream.finish();
-        } finally {
+		try {
+			outputStream = new ZipOutputStream(new NativeFile(new File(output), "rw"), zipModel);
+			InputStream inputStream = new FileInputStream(new File(input));
+			byte[] readBuff = new byte[4096];
+			int readLen = -1;
+			while ((readLen = inputStream.read(readBuff)) != -1) {
+				outputStream.write(readBuff, 0, readLen);
+			}
+
+		} finally {
 			outputStream.close();
 		}
-    }
+	}
 
-    public void unzip(String input, String output, String password) throws ZipException {
-        ZipFile zipFile = new ZipFile(input);
-        if (zipFile.isEncrypted()) {
-            zipFile.setPassword(password);
-        }
-        zipFile.extractAll(output);
-    }
+	public void unzip(String input, String output, String password) throws ZipException {
+		ZipFile zipFile = new ZipFile(input);
+		if (zipFile.isEncrypted()) {
+			zipFile.setPassword(password);
+		}
+		zipFile.extractAll(output);
+	}
 }
