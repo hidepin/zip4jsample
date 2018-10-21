bd29ae2cf7e627db5439c85052afab86ae50cdf0
Hidetoshi Imai <hidepin@gmail.com>
2018-10-21 22:26:46 +0900

    update.

--- src/main/java/org/func/sample/zip4jsample/ZipLoop.java
+++ src/main/java/org/func/sample/zip4jsample/ZipLoop.java
@@ -4,19 +4,21 @@ import net.lingala.zip4j.util.Zip4jConstants;
 
 public class ZipLoop {
 	public static void main(String[] args) throws Exception {
-		if (args.length != 3) {
-			System.err.println("error");
-			System.exit(1);
-		}
+
 		String source = args[0];
 		String zipFilePath = args[1];
 		String password = "password";
 		String method = args[2];
+		int loop = Integer.parseInt(args[3]);
+		int sleep = Integer.parseInt(args[4]);
+				
 		
 		if (method.equals("zip")) {
 			new ZipUtil().zip(source, zipFilePath, password);
 		} else if (method.equals("unzip")) {
-			new ZipUtil().unzip(source, zipFilePath, password);
+			for (int i = 0; i < 3; i++) {
+				new ZipThread(source, zipFilePath, password, loop, sleep).start();
+			}
 		}
 	}
 }
